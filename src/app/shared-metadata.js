88704d35c24a08dbd783dad38cd4af2f24be33a2
mshakhriyorov <mshakhriyorov8@gmail.com>
2024-04-09 04:24:36 -0700

    Update shared metadata and remove unnecessary files

--- src/app/shared-metadata.js
+++ src/app/shared-metadata.js
@@ -1,5 +1,5 @@
-export const sharedTitle = 'Onur Şuyalçınkaya'
-export const sharedDescription = 'Software Engineer, DJ, writer, and minimalist, based in Amsterdam, The Netherlands'
+export const sharedTitle = 'IELTS GURUS'
+export const sharedDescription = 'IELTS instructors and learning centres hub'
 export const sharedImage = {
   width: 1200,
   height: 630,
