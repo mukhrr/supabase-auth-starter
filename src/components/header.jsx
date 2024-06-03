f70dd7e9f41519501e3ee950c6035c6e8758368c
mshakhriyorov <mshakhriyorov8@gmail.com>
2024-06-03 21:23:20 +0500

    +
    Signed-off-by: mshakhriyorov <mshakhriyorov8@gmail.com>

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,16 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react'
-import { Menu } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
 import CommandMenu from '@/components/command-menu'
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function Header({ children }) {
   const navRef = useRef(null)
   const [scroll, setScroll] = useState(false)
-  const [open, setOpen] = useState(false)
 
-  console.log(open)
   useEffect(() => {
     const intersectionObserver = new IntersectionObserver(
       (entries) => {
@@ -38,7 +33,7 @@ export default function Header({ children }) {
       <div ref={navRef}></div>
       <div
         className={cn(
-          'fixed right-0 top-0 z-50 flex h-16 w-full items-center justify-between border-b bg-background xl:w-[calc(100%-240px)]',
+          'fixed top-0 z-50 flex h-16 w-full items-center justify-between border-b bg-background xl:w-[calc(100%-240px)]',
           {
             'h-12': scroll
           }
